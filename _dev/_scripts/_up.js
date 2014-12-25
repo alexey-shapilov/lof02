774eecb8f15e64d98031b12bb33c78af0b64e68c
Alexey Shapilov <alexey.shapilov@gmail.com>
2014-12-25 12:36:56 +0300

    Добавил ссылки на домашнюю страницу для логотипа, ссылки на социальные сети. Создал объект BtnUp

--- _dev/_scripts/_up.js
+++ _dev/_scripts/_up.js
@@ -29,121 +29,145 @@
         return i.style.opacity === "0.5";
     }
 
-    function Up(prop) {
-        return this
+    function BtnUp(options) {
+        var
+            default_options = {};
+
+        if (!options.btn) {
+            return false;
+        }
+        extend(options, default_options);
+
+        this.options = options;
+        this.btn = getElementsByClassName(document.body, options.btn)[0];
+        this.btn.onclick = function () {
+            getElementsByClassName(document.body, options.contentScroll)[0].scrollIntoView(true);
+        };
+
+        this.btnUpArrowTop = getElementsByClassName(this.btn, options.arrow.top)[0];
+        this.btnUpArrowBottom = getElementsByClassName(this.btn, options.arrow.bottom)[0];
+
+        this.intervalShow = 0;
+        this.intervalHide = 0;
     }
+
+    BtnUp.prototype.position = function (position) {
+        if ('left' in position) this.btn.style.left = position.left;
+        if ('right' in position) this.btn.style.right = position.right;
+        if ('top' in position) this.btn.style.top = position.top;
+        if ('bottom' in position) this.btn.style.bottom = position.bottom;
+    };
+
+    BtnUp.prototype.fadeIn = function (duration) {
+        var
+            opacity = 0,
+            deltaOpacity = 1 / (duration / 40),
+            supportOpacity = supports_opacity(),
+            self = this;
+
+        if (supportOpacity) {
+            opacity = +this.btn.style.opacity
+        } else {
+            /alpha\(.*opacity\s*=\s*(\d+\.?\d*).*\)/i.test(this.btn.currentStyle.filter);
+            opacity = parseFloat(RegExp.$1) / 100;
+        }
+
+        clearInterval(this.intervalHide);
+        this.intervalShow = setInterval(function () {
+            if (opacity < 1) {
+                opacity += deltaOpacity;
+                if (supportOpacity) {
+                    self.btn.style.opacity = opacity;
+                } else {
+                    self.btn.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
+                    self.btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
+                    self.btnUpArrowBottom.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
+                }
+            } else {
+                clearInterval(self.intervalShow);
+            }
+        }, 40);
+        this.visibleBtnUp = true;
+    };
+
+    BtnUp.prototype.fadeOut = function (duration) {
+        var
+            deltaOpacity = 1 / (duration / 40),
+            supportOpacity = supports_opacity(),
+            opacity = 0,
+            self = this;
+
+        if (supportOpacity) {
+            opacity = this.btn.style.opacity
+        } else {
+            /alpha\(.*opacity\s*=\s*(\d+\.?\d*).*\)/i.test(this.btn.currentStyle.filter);
+            opacity = parseFloat(RegExp.$1) / 100;
+        }
+
+        this.visibleBtnUp = false;
+        clearInterval(this.intervalShow);
+        this.intervalHide = setInterval(function () {
+            if (opacity > 0.1) {
+                opacity -= deltaOpacity;
+                if (supportOpacity) {
+                    self.btn.style.opacity = opacity;
+                } else {
+                    self.btn.style.filter = self.btnUpArrowTop.style.filter = self.btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
+                }
+            } else {
+                self.btn.style.opacity = 0;
+                self.btn.style.filter = self.btnUpArrowTop.style.filter = self.btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=0)';
+                clearInterval(self.intervalHide);
+            }
+        }, 40);
+    };
 
     var
         html,
         body,
         btnUp,
-        btnUpArrowTop,
-        btnUpArrowBottom,
         content,
-        footer,
-        htmlWidth,
-        visibleUp,
-        intervalShow,
-        intervalHide;
+        footer;
 
     window.onload = function () {
         html = document.documentElement;
         body = document.body;
-        btnUp = getElementsByClassName(body, 'b-up')[0];
-        btnUpArrowTop = getElementsByClassName(btnUp, 'b-up__arrow-top')[0];
-        btnUpArrowBottom = getElementsByClassName(btnUp, 'b-up__arrow-bottom')[0];
+        btnUp = new BtnUp({
+            btn: 'b-up',
+            arrow: {
+                top: 'b-up__arrow-top',
+                bottom: 'b-up__arrow-bottom'
+            },
+            contentScroll: 'content-wrapper'
+        });
+
         content = getElementsByClassName(body, 'b-head')[0];
         footer = getElementsByClassName(body, 'b-footer')[0];
-        htmlWidth = html.clientWidth;
-        visibleUp = false;
 
         window.onscroll = function () {
             var
                 scrollV = 147,
                 scrolled = window.pageYOffset || html.scrollTop,
-                btnTop = html.clientHeight - btnUp.offsetHeight - 20,
+                btnTop = html.clientHeight - btnUp.btn.offsetHeight - 20,
                 restScroll = html.scrollHeight - scrolled - html.clientHeight,
-                supportOpacity = supports_opacity();
+                btnPos = {};
+
 
             if (scrolled > scrollV) {
-                //btnUp.style.display = 'block';
-                btnUp.style.right = (htmlWidth - content.offsetWidth) / 2 + 10 + 'px';
+                btnPos.right = (html.clientWidth - content.offsetWidth) / 2 + 10 + 'px';
                 if (restScroll > footer.offsetHeight) {
-                    btnUp.style.top = btnTop + 'px';
+                    btnPos.top = btnTop + 'px';
                 } else {
-                    btnUp.style.top = btnTop - (footer.offsetHeight - restScroll) + 'px';
+                    btnPos.top = btnTop - (footer.offsetHeight - restScroll) + 'px';
                 }
+                btnUp.position(btnPos);
 
-                if (!visibleUp) {
-                    var
-                        duration = 500,
-                        opacity = 0,
-                        deltaOpacity = 1 / (duration / 40);
-
-                    if (supportOpacity) {
-                        opacity = +btnUp.style.opacity
-                    } else {
-                        /alpha\(.*opacity\s*=\s*(\d+\.?\d*).*\)/i.test(btnUp.currentStyle.filter);
-                        opacity = parseFloat(RegExp.$1) / 100;
-                    }
-
-                    clearInterval(intervalHide);
-                    intervalShow = setInterval(function () {
-                        if (opacity < 1) {
-                            opacity += deltaOpacity;
-                            if (supportOpacity) {
-                                btnUp.style.opacity = opacity;
-                            } else {
-                                btnUp.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                                btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                                btnUpArrowBottom.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                            }
-                        } else {
-                            clearInterval(intervalShow);
-                        }
-                    }, 40);
-                    visibleUp = true;
+                if (!btnUp.visibleBtnUp) {
+                    btnUp.fadeIn(500);
                 }
-
-            } else if (scrolled <= scrollV && visibleUp) {
-                var
-                    duration = 500,
-                    deltaOpacity = 1 / (duration / 40);
-
-                if (supportOpacity) {
-                    opacity = +btnUp.style.opacity
-                } else {
-                    /alpha\(.*opacity\s*=\s*(\d+\.?\d*).*\)/i.test(btnUp.currentStyle.filter);
-                    opacity = parseFloat(RegExp.$1) / 100;
-                }
-
-                visibleUp = false;
-                clearInterval(intervalShow);
-                intervalHide = setInterval(function () {
-                    //console.log(opacity);
-                    if (opacity > 0.1 ) {
-                        opacity -= deltaOpacity;
-                        if (supportOpacity) {
-                            btnUp.style.opacity = opacity;
-                        } else {
-                            btnUp.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                            btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                            btnUpArrowBottom.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + (opacity * 100) + ')';
-                        }
-                        //console.log(opacity);
-                    } else {
-                        btnUp.style.opacity = 0;
-                        btnUp.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=0)';
-                        btnUpArrowTop.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=0)';
-                        btnUpArrowBottom.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=0)';
-                        clearInterval(intervalHide);
-                    }
-                }, 40);
+            } else if (scrolled <= scrollV && btnUp.visibleBtnUp) {
+                btnUp.fadeOut(500);
             }
         };
-
-        btnUp.onclick = function () {
-            getElementsByClassName(body,'content-wrapper')[0].scrollIntoView(true);
-        }
     }
 })();
